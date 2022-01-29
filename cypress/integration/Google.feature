4af4164b59b9acb0466273a4ee2800eb10bed031
tyler <tylerknapp25@gmail.com>
2022-01-29 18:08:37 -0500

    Basic test passing POC

--- cypress/integration/Google.feature
+++ cypress/integration/Google.feature
@@ -2,7 +2,6 @@ Feature: Google Main Page
 
   I want to open a search engine
   
-  @focus
   Scenario: Opening a search engine page
     Given I open Google page
 
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,11 +12,10 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
+
+
 const cucumber = require('cypress-cucumber-preprocessor').default
+
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
   
